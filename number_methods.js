754ec2e98c83958cb20dd57715785b4999ef5446
IamSila <silamulingi@gmail.com>
2026-05-02 09:37:02 +0300

    Number methods.js

--- number_methods.js
+++ number_methods.js
@@ -40,3 +40,34 @@ let float_to_int = parseInt(number_2);
 console.log(float_to_int);
 
 
+
+
+
+// toFixed()
+let num = 10.345;
+let num_to_five_dp = num.toFixed(5);
+let num_to_precision = num.toPrecision(8);
+let num_valueof = (num * 3).valueOf().toFixed(3).toString(); // string
+console.log(`num ${num} valueOf is ${num_valueof}`);
+
+
+
+let num_isinteger = Number.isInteger(10);
+let num_isNaN = Number.isNaN("string");
+let num_isinfinite = Number.isFinite(NaN);
+let num_safe = Number.isSafeInteger(10);
+let num_Notsafe = Number.isSafeInteger(12345678901234567890);
+console.log(num_Notsafe);
+
+let x = Number.NEGATIVE_INFINITY;
+console.log(x);
+
+
+
+
+
+console.log("We are 'Vikings'");
+console.log('We are "Vikings"');
+
+/** the escape character is the backslash \ */ 
+console.log("We are the \"Vikings\"");
